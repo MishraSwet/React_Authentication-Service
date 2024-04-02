a8067a86686d133d1d2a197747cbb5c3673685b0
MishraSwet <swetaparnamishraofficial@gmail.com>
2024-04-02 23:44:48 +0530

    Added Login and Signup Pages

---
+++ React_Auth_Service/pages/Login.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Login:React.FC = () => {
+    return (
+        <div>Login PAGE</div>
+    );
+}
+
+export default Login
---
+++ React_Auth_Service/pages/SignUp.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Signup: React.FC = () => {
+    return (
+        <div>SignUP</div>
+    );
+}
+
+export default Signup;
