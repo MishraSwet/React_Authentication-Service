6beafb09153a19dd1a7dcc1945519d1a69209749
MishraSwet <swetaparnamishraofficial@gmail.com>
2024-04-03 23:56:13 +0530

    Added State And Logic to Login Component

--- React_Auth_Service/pages/Login.tsx
+++ React_Auth_Service/pages/Login.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Login:React.FC = () => {
+const Login: React.FC = () => {
+    const [login, setLogin] = useState("");
+    const [password, setPassword] = useState("");
+    const handleLogin = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setLogin(e.target.value);
+    }
+    const handlePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value);
+    }
     return (
+        <>
         <div>Login</div>
+            <form>
+                <label>Login</label>
+                <input type="text" value={login} onChange={handleLogin}></input>
+                <label>Password</label>
+                <input type="text" value={password} onChange={handlePassword}></input>
+            </form>
+        </>
     );
 }
 
